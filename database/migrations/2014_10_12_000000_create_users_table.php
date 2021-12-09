40c77528cff35a604cf2b71b64ccd57b232b1b49
Alejandro Ferrer <amfale@hotmail.com>
2021-12-09 11:32:33 -0300

    migraciones 3, agregando y modificando columnas en tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');//varchar, 255 carateres, text para mas caracteres $table->text()
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();//timestamp:para guardar fechas, nullable: porq puede quedar vacio
-            $table->string('password');
-            $table->string('avatar');
+            $table->string('password');            
             $table->rememberToken();//este metodo crea un varchar para alamcenar un token cuando el usuario marca "mantener la sesion iniciada"
             $table->timestamps();//crea 2 columnas, created_at, update_at
         });
